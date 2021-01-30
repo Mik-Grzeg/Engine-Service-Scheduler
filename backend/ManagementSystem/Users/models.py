1557285bc63f1a79315906b505726086531784f7
mikolajgrzegorzek <mikgrzeg@icloud.com>
2021-01-30 20:48:32 +0100

    Django project initialization. Created Users application, it already contain code of custom user model and manager. Wrote some test, basically focused on checking creation of custom user, which most of the logic is in manager.

---
+++ backend/ManagementSystem/Users/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth import password_validation
+
+from django.core.validators import RegexValidator
+
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class UserManager(BaseUserManager):
+    """
+    Custom user model manager where email address is the unique identifier for authentication instead of username.
+    """
+
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with given credentials that are mandatory and extra_fields that are supplementary.
+        """
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+
+        # might throw a ValidationError
+        password_validation.validate_password(password, user)
+        user.set_password(password)
+
+        # might throw a ValidationError
+        user.clean_fields()
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password,  **extra_fields):
+        user = self.create_user(email, password, **extra_fields)
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user model that takes email as username field. Additionally has some more useful fields.
+    """
+    email = models.EmailField(_('email address'), unique=True)
+
+    # Validator for names that takes only letters
+    letters_validator = RegexValidator(r'^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]*$', 'Only letters are allowed.')
+
+    first_name = models.CharField(max_length=40, validators=[letters_validator,])
+    last_name = models.CharField(max_length=40, validators=[letters_validator,])
+    work_phone = PhoneNumberField()
+
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'work_phone']
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+    @property
+    def is_staff(self):
+        return self.is_superuser
---
+++ backend/ManagementSystem/Users/tests/test_models.py
@@ -0,0 +1,67 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+
+
+class UserTestCase(TestCase):
+    user = get_user_model()
+    password = '$Kasdm123ca'
+    email = 'email@mail.com'
+    first_name = 'firśt'
+    last_name = 'ląst'
+    work_phone = '+48123456789'
+
+    def test_create_valid_user(self):
+        user = self.user.objects.create_user(email=self.email, password=self.password,
+                                             first_name=self.first_name, last_name=self.last_name,
+                                             work_phone=self.work_phone)
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+        # Checking if there were no mistakes
+        self.assertEqual(user.email, self.email)
+        self.assertEqual(user.first_name, self.first_name)
+        self.assertEqual(user.last_name, self.last_name)
+        self.assertEqual(user.work_phone, self.work_phone)
+
+    def test_model_str(self):
+        user = self.user.objects.create_user(email=self.email, password=self.password,
+                                             first_name=self.first_name, last_name=self.last_name,
+                                             work_phone=self.work_phone)
+        self.assertEqual(str(user), 'firśt ląst')
+
+    def test_create_blank_user(self):
+        with self.assertRaises(TypeError):
+            self.user.objects.create_user()
+
+    def test_create_invalid_email(self):
+        with self.assertRaises(ValidationError):
+            self.user.objects.create_user(email='', password=self.password,
+                                          first_name=self.first_name, last_name=self.last_name,
+                                          work_phone=self.work_phone)
+            self.user.objects.create_user(email='emailaddress', password=self.password,
+                                          first_name=self.first_name, last_name=self.last_name,
+                                          work_phone=self.work_phone)
+            self.user.objects.create_user(email='email@@mail.com', password=self.password,
+                                          first_name=self.first_name, last_name=self.last_name,
+                                          work_phone=self.work_phone)
+            self.user.objects.create_user(email='email@mail', password=self.password,
+                                          first_name=self.first_name, last_name=self.last_name,
+                                          work_phone=self.work_phone)
+
+    def test_create_invalid_names(self):
+        with self.assertRaises(ValidationError):
+            self.user.objects.create_user(email=self.email, password=self.password,
+                                          first_name='123name', last_name='ąż@#$123456789123456789123456789123456789123456789',
+                                          work_phone=self.work_phone)
+
+    def test_create_valid_superuser(self):
+        User = get_user_model()
+        user = User.objects.create_superuser(email=self.email, password=self.password,
+                                             first_name=self.first_name, last_name=self.last_name,
+                                             work_phone=self.work_phone )
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+
