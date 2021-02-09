6cf455c736db5423b04201d3ef74a7cffa35d78b
mikolajgrzegorzek <mikgrzeg@icloud.com>
2021-02-09 19:00:10 +0100

    Checking if tests pass not at my local machine

--- backend/ManagementSystem/Users/tests/test_views.py
+++ backend/ManagementSystem/Users/tests/test_views.py
@@ -147,9 +147,9 @@ class UserViewSetTest(APITestCase):
         # hence after logging in, hash changes, so the token is no longer valid.
         self.assertEqual(response.data.get('message'), 'That link is broken or it has already been used.')
         self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
-    """
+
     def test_registering_activating_and_setting_password(self):
-        #Testing activation account by emailed link, and setting password
+        """Testing activation account by emailed link, and setting password"""
         login_url = '/auth/login/'
         login_response = self.client.post(login_url, {"email": self.admin.email, "password": self.admin_passwd},
                                           format='json')
@@ -166,10 +166,11 @@ class UserViewSetTest(APITestCase):
                                             data=self.valid_regular_user,
                                             format='json'
                                            )
-        print(self.User.objects.all())
-        user = self.User.objects.get(email=register_response.data.get('email'))
+        #print(self.User.objects.all())
+        #user = self.User.objects.get(email=register_response.data.get('email'))
         self.assertEqual(register_response.status_code, status.HTTP_201_CREATED)
         self.assertDictEqual(register_response.data, self.valid_regular_user)
+        '''
         self.assertFalse(user.is_verified)
 
         # creating uidb64 and token, because normally it's sent in mail content.
@@ -202,7 +203,7 @@ class UserViewSetTest(APITestCase):
         self.assertDictEqual(set_password_response.data, {'message': 'Password has been updated successfully.'})
         user.refresh_from_db()
         self.assertTrue(user.check_password(new_password))
-        """
+       '''
 
     def test_forgot_password_unverified_user(self):
         user = self.User.objects.create_user(**self.valid_regular_user)
