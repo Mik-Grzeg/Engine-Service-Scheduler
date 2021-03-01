af03ec441f083c42bf41e15b88faf0fe1688baf6
DawidKrzoska <dawidwilk19999@gmai.com>
2021-03-01 19:47:39 +0100

    =comapny list

--- frontend/generatorlosowychserisow/src/reducers/companyReducer.js
+++ frontend/generatorlosowychserisow/src/reducers/companyReducer.js
@@ -6,9 +6,11 @@ const defaultState = {
     { name: "cos tam 2", id: 2 },
     { name: "cos tam 3", id: 3 },
   ],
+  isCompanyDowloaded: false,
   company: {
     name: "NAME",
-    localisation: "LOCATISATION",
+    contact: "+48000000000",
+    installation_set: [{ installation_name: "installation_name 1" }],
   },
 };
 
@@ -18,9 +20,41 @@ const companyReduser = (state = defaultState, action) => {
   }
 
   if (action.type === DOWNLOAD_COMPANY) {
-    return { ...state, company: action.payload };
+    return {
+      ...state,
+      isCompanyDowloaded: true,
+      company: {
+        ...state.company,
+        name: action.payload.name,
+        contant: action.payload.contant,
+        installation_set: [
+          {
+            ...state.company.installation_set,
+            installation_name: action.payload.installation_set,
+          },
+        ],
+      },
+    };
   }
   return { ...state };
 };
 
 export default companyReduser;
+
+{
+  /*export function rootLevelReducer(state, action){
+    return {
+        ...state,
+        firstLevel: {
+            ...state.firstLevel,
+            secondLevel: {
+                ...state.firstLevel.secondLevel,
+                thirdLevel: {
+                    ...state.firstLevel.secondLevel.thirdLevel,
+                    property1: action.data
+                }
+            }
+        }
+    }
+} */
+}
